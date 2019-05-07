bb485a3b3b2d763f100d7c5e9979fe2c5d0ba5f2
asdwsxjilh <381206978@qq.com>
2019-05-07 08:14:53 +0800

    1.增加原型模式深拷贝

--- app/src/main/java/com/yexm/pattern/create/prototype/Prototype.java
+++ app/src/main/java/com/yexm/pattern/create/prototype/Prototype.java
@@ -1,5 +1,11 @@
 package com.yexm.pattern.create.prototype;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * 原型模式
  */
@@ -8,5 +14,17 @@ public class Prototype implements Cloneable{
     @Override
     protected Prototype clone() throws CloneNotSupportedException {
         return (Prototype)super.clone();
+    }
+
+    public Prototype deepClone() throws IOException, ClassNotFoundException{
+        //写入当前对象的二进制流
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(this);
+
+        //读取二进制对象流
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        return (Prototype) ois.readObject();
     }
 }
